ed7182a392145ea392873517efd30e31b1c6e9ad
Seattle Academy <seattleacademy@users.noreply.github.com>
2020-03-20 13:24:40 -0700

    Update script from codepen

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
 console.clear()
 console.log("hello world")
+
+$("input").change(onChange);
+
+function onChange(evt) {
+  let correct = $(this).data("correct");
+  let response = $(this).val();
+
+  console.log(md5(response));
+  if (correct == md5(response)) {
+    $(this)
+      .removeClass("incorrect")
+      .addClass("correct");
+    let theScore = Number($("#score").text());
+    theScore = theScore + 1;
+    $("#score").text(theScore);
+  } else {
+   
+    $(this)
+      .removeClass("correct")
+      .addClass("incorrect");
+  }
+}
